672f87b85bcca07ecc153b5e9391693b696900e1
vibhapatipandey <141785158+vibhapatipandey@users.noreply.github.com>
2023-09-21 16:07:18 -0400

    Update UserRepository.java

--- src/main/java/com/app/user/login/repository/UserRepository.java
+++ src/main/java/com/app/user/login/repository/UserRepository.java
@@ -26,7 +26,7 @@ public class UserRepository {
 		@Override
 		public User mapRow(ResultSet rs, int rowNum) throws SQLException {
 
-			return User.builder().userID(rs.getInt("USER_ID")).userName(rs.getString("USERNAME"))
+			return User.builder().userID(rs.getInt("USERID")).userName(rs.getString("USERNAME"))
 					.password(rs.getString("PASSWORD")).userFName(rs.getString("USER_FNAME"))
 					.userLName(rs.getString("USER_LNAME")).userDOB(rs.getDate("USER_DOB").toString())
 					.dptName(rs.getString("USER_DPT_NAME")).traningName(rs.getString("USER_TRAINING_NAME")).build();
@@ -38,12 +38,30 @@ public class UserRepository {
 	public boolean isUserExists(String username) {
         String sql = "SELECT count(*) FROM USERS WHERE userName = ?";
 	boolean result = false;
-	int count = jdbcTemplate.queryForObject( sql, new Object[] { username }, Integer.class);
+	var count = jdbcTemplate.queryForObject( sql, new Object[] { username }, Integer.class);
 	if (count > 0) {
 		result = true;
 	}
 	return result;
   }
+	
+	
+
+	/*public boolean isUserExists(User user) {
+		String username= user.getUserName(); 
+		String password= user.getPassword();
+		System.out.println("username  "+username +"    password  "+password);
+        String sql = "SELECT username, password FROM USERS WHERE userName = ? and password=?";
+	boolean result = false;
+	var dbUser =jdbcTemplate.queryForObject( sql, new Object[] { username, password }, new UserRowMapper());
+	System.out.println("==========="+ dbUser.getUserName());
+	if (dbUser.getUserName().equals(username)&&dbUser.getPassword().equals(password)) {
+		result = true;
+	}
+	return result;
+  }*/
+	
+	
 	
 	public int createUser(User user) {
         return jdbcTemplate.update("insert into USERS (user_fname, user_lname, user_dob,username,password,user_dpt_name,user_training_name) " 
